dcabd306e2f9320a31842c847d81cb647f498b1c
shenfan0613 <shen.1450@osu.edu>
2022-05-18 13:10:01 -0400

    13

--- index.js
+++ index.js
@@ -11,6 +11,12 @@ addEventListener('fetch', event => {
 });
 
 const FORM_URL = "https://aiwriter.pages.dev/"
+const RESLT_URL = ""
+const useCaseJobDescriptionId = '60586b31cdebbb000c21058d'
+const languageIdEnglish = '607adac76f8fe5000c1e636d'
+const toneIdConvincing = '60572a639bdd4272b8fe358b'
+const RYTE_API_KEY = 'QHPVUE6PN9JZOBEDLRVQH'
+const RYTE_API_URL = 'https://api.rytr.me/v1'
 
 async function handleRequest(request) {
     const url = new URL(request.url)
@@ -47,31 +53,23 @@ const submitHandler = async request => {
         }
     }
     //console.log(reqBody)
-    await createAirtableRecord(reqBody)
+
+    await ryte({userId:userId,jobTitle:jobType})
+    //await createAirtableRecord({body:reqBody,tableName:"Input"})
+    // const reqBody2 = {
+    //     fields: {
+    //         "userId": userId,
+    //         "content": res.data[0].text,
+    //     }
+    // }
+    //await createAirtableRecord()
     return Response.redirect(FORM_URL)
 }
 
-const createRecord = body => {
-    base('Input').create([
-        {
-            "fields": {
-                "userId": body.fields.userId,
-                "Job Description": body.fields.jobType
-            }
-        }
-    ], function(err, records) {
-        if (err) {
-            console.error(err);
-            return;
-        }
-        records.forEach(function (record) {
-            console.log(record.getId());
-        });
-    });
-};
-const createAirtableRecord = body => {
-    console.log(JSON.stringify(body))
-    return fetch(`https://api.airtable.com/v0/${AIRTABLE_BASE_ID}/${encodeURIComponent(airtableName)}`, {
+
+async function createAirtableRecord ({body,tableName}) {
+    //console.log(JSON.stringify(body))
+    return fetch(`https://api.airtable.com/v0/${AIRTABLE_BASE_ID}/${encodeURIComponent(tableName)}`, {
         method: 'POST',
         body: JSON.stringify(body),
         headers: {
@@ -80,3 +78,41 @@ const createAirtableRecord = body => {
         }
     })
 }
+
+
+// ryte
+async function ryte({userId, jobTitle}) {
+    const reqBody = {
+            'languageId': languageIdEnglish,
+            'toneId': toneIdConvincing,
+            'useCaseId':useCaseJobDescriptionId,
+            'inputContexts': {"JOB_ROLE_LABEL": jobTitle},
+            'variations': 1,
+            'userId': userId,
+            'format': 'text',
+    }
+    let response = await fetch(`https://api.rytr.me/v1/ryte`, {
+        method: 'POST',
+        headers: {
+            'Authentication': `Bearer ${RYTE_API_KEY}`,
+            'Content-type': `application/json`
+        },
+        body:JSON.stringify(reqBody)
+    })
+    let data1 = await response.json()
+    console.log(data1.data[0].text)
+    const reqBody2 = {
+        fields: {
+            "userId": userId,
+            "content": data1.data[0].text,
+        }
+    }
+    await createAirtableRecord ({body:reqBody2,tableName:"paragragh"})
+
+    return null
+
+}
+// (async () => {
+//     let data = await useCaseDetailById(useCaseJobDescriptionId)
+//     console.log(data)
+// })()
