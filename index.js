bbbd355b275bba94a5b0f00684e81314d5b86287
shenfan0613 <shen.1450@osu.edu>
2022-05-17 23:57:24 -0400

    2

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
+const AIRTABLE_BASE_ID = "appNzuUysRFnMK56E"
+let airtableName = "Input"
 addEventListener('fetch', event => {
     event.respondWith(handleRequest(event.request));
 });
 
-const FORM_URL = "https://airtable-form-example.pages.dev"
+const FORM_URL = "https://aiwriter.pages.dev/"
 
 async function handleRequest(request) {
     const url = new URL(request.url)
@@ -12,4 +14,41 @@ async function handleRequest(request) {
     }
 
     return Response.redirect(FORM_URL)
+}
+const submitHandler = async request => {
+    if (request.method !== "POST") {
+        return new Response("Method Not Allowed", {
+            status: 405
+        })
+    }
+
+    const body = await request.formData();
+
+    const {
+        userId,
+        jobType,
+    } = Object.fromEntries(body)
+
+    // The keys in "fields" are case-sensitive, and
+    // should exactly match the field names you set up
+    // in your Airtable table, such as "First Name".
+    const reqBody = {
+        fields: {
+            "userId": userId,
+            "Job Description": jobType,
+        }
+    }
+
+    await createAirtableRecord(reqBody)
+    return Response.redirect(FORM_URL)
+}
+const createAirtableRecord = body => {
+    return fetch(`https://api.airtable.com/v0/${AIRTABLE_BASE_ID}/${encodeURIComponent(airtableName)}`, {
+        method: 'POST',
+        body: JSON.stringify(body),
+        headers: {
+            Authorization: `Bearer ${AIRTABLE_API_KEY}`,
+            'Content-type': `application/json`
+        }
+    })
 }
