77081b36204093cb8bc0d3b532e0345538b6ade5
shenfan0613 <shen.1450@osu.edu>
2022-05-18 17:24:15 -0400

    fail page

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const axios = require('axios')
 let Airtable = require('airtable');
 let base = new Airtable({apiKey: 'keyVDFnHwMynxFqLv'}).base('appNzuUysRFnMK56E');
+const USAGE_LIMIT = 4;
 
 
 const AIRTABLE_BASE_ID = "appNzuUysRFnMK56E"
@@ -12,11 +13,14 @@ addEventListener('fetch', event => {
 
 const FORM_URL = "https://aiwriter.pages.dev/"
 const RESULT_URL = "https://aiwriter.pages.dev/result"
+const FAIL_URL = "https://aiwriter.pages.dev/fail"
 const useCaseJobDescriptionId = '60586b31cdebbb000c21058d'
 const languageIdEnglish = '607adac76f8fe5000c1e636d'
 const toneIdConvincing = '60572a639bdd4272b8fe358b'
 const RYTE_API_KEY = 'QHPVUE6PN9JZOBEDLRVQH'
 const RYTE_API_URL = 'https://api.rytr.me/v1'
+
+
 
 async function handleRequest(request) {
     const url = new URL(request.url)
@@ -40,9 +44,6 @@ const submitHandler = async request => {
         jobType,
     } = Object.fromEntries(body)
 
-    console.log(userId)
-    console.log(jobType)
-
     // The keys in "fields" are case-sensitive, and
     // should exactly match the field names you set up
     // in your Airtable table, such as "First Name".
@@ -53,13 +54,36 @@ const submitHandler = async request => {
         }
     }
     //console.log(reqBody)
-
-    await ryte({userId:userId,jobTitle:jobType})
-    await createAirtableRecord({body:reqBody,tableName:"Input"})
-    return Response.redirect(RESULT_URL)
+    let usage = await checkUsage({userId:userId})
+    if(usage <= USAGE_LIMIT){
+        await ryte({userId:userId,jobTitle:jobType})
+        await createAirtableRecord({body:reqBody,tableName:"Input"})
+    }else{
+        return Response.redirect(FAIL_URL)
+    }
+    return Response.redirect(FORM_URL)
 }
 
-
+async function checkUsage({userId}){
+    let response = await fetch('https://api.airtable.com/v0/appNzuUysRFnMK56E/paragragh', {
+        method:'GET',
+        headers: {
+            Authorization: `Bearer ${AIRTABLE_API_KEY}`,
+            'Content-type': `application/json`
+        }
+    });
+    //console.log(await response.json())
+    let res = await response.json()
+    let records = res.records
+    let count = 0;
+    //console.log(res)
+    for (const entry of records){
+        if(entry.fields.userId == userId){
+            count += 1;
+        }
+    }
+    return count;
+}
 async function createAirtableRecord ({body,tableName}) {
     //console.log(JSON.stringify(body))
     return fetch(`https://api.airtable.com/v0/${AIRTABLE_BASE_ID}/${encodeURIComponent(tableName)}`, {
