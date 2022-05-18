fc5254ac6b03e8570e32841c25f6e5aefb6daade
shenfan0613 <shen.1450@osu.edu>
2022-05-18 13:25:39 -0400

    16

--- public/result.js
+++ public/result.js
@@ -1,8 +1,8 @@
-let res = fetch('https://api.airtable.com/v0/appNzuUysRFnMK56E/paragragh/recxVGRmwfazKNXdW', {
+let res = await fetch('https://api.airtable.com/v0/appNzuUysRFnMK56E/paragragh/recxVGRmwfazKNXdW', {
     headers: {
         'Authorization': 'Bearer keyVDFnHwMynxFqLv'
     }
 });
 //let result = res.json()
-console.log(res.getBody)
+console.log(res.text())
 //document.getElementById("result").innerHTML = result
